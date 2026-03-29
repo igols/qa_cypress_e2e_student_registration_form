a10575ca2f462c2b420c4ff66e8fa41ce7e1e987
Igor <igor.olshanskyi@gmail.com>
2026-03-29 19:41:06 +0300

    add test

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -28,22 +28,24 @@ describe('Student Registration page', () => {
     cy.get('#currentAddress').type('Ukraine, Kyiv, Khreshchatyk 1');
     cy.get('#state').click();
     cy.contains('NCR').click();
+    cy.get('#city').click();
     cy.get('#submit').click();
 
     cy.get('.modal-content').should('be.visible');
-    cy.get('.modal-header')
-      .should('have.text', 'Thanks for submitting the form');
     cy.get('.table-responsive').within(() => {
       cy.contains('td', 'Student Name')
         .next().should('have.text', 'Name LastName');
       cy.contains('td', 'Student Email')
         .next().should('have.text', 'Name@example.com');
+      cy.contains('td', 'Gender').next().should('have.text', 'Male');
       cy.contains('td', 'Mobile').next().should('have.text', '1234567890');
       cy.contains('td', 'Date of Birth')
         .next().should('have.text', '15 May,1995');
       cy.contains('td', 'Subjects').next().should('have.text', 'Maths');
       cy.contains('td', 'Address')
         .next().should('have.text', 'Ukraine, Kyiv, Khreshchatyk 1');
+      cy.contains('td', 'State and City')
+        .next().should('have.text', 'NCR Delhi');
     });
     cy.get('#closeLargeModal').click();
   });
